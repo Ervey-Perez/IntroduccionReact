b15619183aa2152115c82f98f7f55ad53ce8cc29
Ervey <erveyperez23@gmail.com>
2022-05-04 18:46:54 -0600

    Mantener estado de todos los cuadrados en el componente Board.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state= {
-            value: null,
-        };
-    }
-
+class Square extends React.Component {  
     render() {
       return (
-        <button className="square" onClick={() => this.setState({value: 'X'})}>
+        <button className="square" 
+        onClick={() => this.props.onClick()}
+        >
           {this.props.value}
         </button>
       );
@@ -27,8 +22,19 @@ class Square extends React.Component {
         };        
     }
 
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-      return <Square value = {this.state.squares[i]} />;
+      return (
+      <Square 
+      value = {this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+       />
+      );
     }
   
     render() {
